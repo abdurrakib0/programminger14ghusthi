b1568e236eb8568376ba73d8eb3e2109cd642d36
arafatrakib1 <msaraib3@gmail.com>
2017-12-30 19:05:56 +0600

    Name initial finding - 2nd way

---
+++ nameInitial1.js
@@ -0,0 +1,14 @@
+function getInitials(name) {
+  var parts = name.split(' '); // it will spilit the whole string where it will find ' ' that means space, then will save it to the parts
+  console.log(parts) // here we can see what the spilit method has done
+  var initials = '' // declared a new var to store initials
+  for (var i = 0; i < parts.length; i++) { // this for loop will run untill the length of the parts
+    if (parts[i].length > 0 && parts[i] != '') {
+      initials += parts[i][0].toUpperCase()na
+    }
+  }
+  return initials
+
+}
+
+console.log(getInitials("arafat rakib hasan"))
